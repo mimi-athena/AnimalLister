24a98f9e9cfbf47cd0d04aafcc5a3445a1dccb04
madison <padregirl91@yahoo.com>
2015-09-17 13:06:39 -0700

    implemented the URLImage in storyboard and view controller

--- ImaginaryAnimalsLister/DetailViewController.swift
+++ ImaginaryAnimalsLister/DetailViewController.swift
@@ -15,7 +15,7 @@ class DetailViewController: UIViewController {
     @IBOutlet weak var heightLabel: UILabel!
     @IBOutlet weak var locationLabel: UILabel!
     @IBOutlet weak var dateLastSeenLabel: UILabel!
-    @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var imageView: URLImageView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -28,19 +28,11 @@ class DetailViewController: UIViewController {
         locationLabel.text = "Location: \(animal!.location)"
         dateLastSeenLabel.text = "Date last seen: \(animal!.dateLastSeen)"
         
-        loadImage()
+        if let animal = animal{
+            imageView.url = animal.imageURL
+        }
+        
+        
     }
     
-   func loadImage() {
-        //is faster loading the image here? Vs up there ^
-        dispatch_async(dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT, 0)) { ()
-            -> Void in
-            if let url = self.animal?.imageURL,
-                let imageData = NSData(contentsOfURL: url) {
-                    dispatch_async(dispatch_get_main_queue(), { () -> Void in
-                        self.imageView.image = UIImage(data: imageData)
-                    })
-            }
-        }
-    }
 }
